a729a065e4be6af4e39afeccc5ba48c2a1f0e73a
nekruzrakhimov <nekruzusa111@gmail.com>
2025-02-27 22:09:10 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+
+def create_user():
+    print("Creating user...")
+
+
+def update_user(name):
+    print("Updating user...")
+
+
+def delete_user(name):
+    print("Deleting user...")
+
+
+def restore_user(name):
+    print("Restoring user...")
