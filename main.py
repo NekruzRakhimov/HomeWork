07cd357061d8f689244b7673c6bce5c15c247999
nekruzrakhimov <nekruzusa111@gmail.com>
2025-02-27 22:56:47 +0500

    added function for printing users

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 
 
 
-
-def create_user():
-    print("Creating user...")
-
-
-def update_user(name):
-    print("Updating user...")
-
-
-def delete_user(name):
-    print("Deleting user...")
-
-
-def restore_user(name):
-    print("Restoring user...")
-
-def login_user(name):
-    print("Logging in...")
+def print_users():
+    print("Hello World")
